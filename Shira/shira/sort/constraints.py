10b9ac9d1ceb887f5e5834fb555e6c10cbc19b61
Aviad Nissel <awbthefirst@gmail.com>
2013-06-27 02:28:32 +0300

    Finished with the constraints module

--- Shira/shira/sort/constraints.py
+++ Shira/shira/sort/constraints.py
@@ -1,5 +1,22 @@
+import random
 
-class HardConstraint():
-    def is_valid(educatives, hugs):
+class HardConstraint(Object):
+    def is_valid(self, educatives, hugs):
         raise NotImplementedError
 
+class SoftConstraint(Object):
+    def __init__(self, score):  
+        self.score = score
+    
+    def calculate_score(self, educatives, hugs):
+        raise NotImplementedError
+
+class RandomSoftConstraint(SoftConstraint):
+    def __init__(self, min_score, max_score):
+        self.score = -1
+        self.min_score = min_score
+        self.max_score = max_score
+        
+    def calculate_random_score(self, educatives, hugs):
+        self.score = random.randint(self.min_score, self.max_score)
+        return self.calculate_score(self.score, educatives, hugs)
