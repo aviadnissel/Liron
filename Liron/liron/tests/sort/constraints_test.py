21c2b2d8a7ca8c431aca5c6593495ed404ec5989
Aviad Nissel <awbthefirst@gmail.com>
2013-07-06 01:13:18 +0300

    Moved the constraint tests to unittest class!

--- Liron/liron/tests/sort/constraints_test.py
+++ Liron/liron/tests/sort/constraints_test.py
@@ -2,6 +2,8 @@ from liron.pojos.model import *
 from liron.pojos.constants import *
 from liron.sort.default_constraints import *
 
+import unittest
+
 def create_seminar(num_of_camps, num_of_hugs):
     seminar = Seminar()
     seminar.name = 'Seminar'
@@ -16,105 +18,111 @@ def create_seminar(num_of_camps, num_of_hugs):
         hug.camp = seminar.camps[i % num_of_camps]
         
     return seminar
-def vegetarian_test():
-    aviad = Educative()
-    aviad.first_name = 'Aviad'
-    aviad.food = VEGETARIAN
-    
-    seminar = create_seminar(1, 1)
-    hug = seminar.camps[0].hugs[0]
-    hug.food = MEAT
-    aviad.hug = hug
-    
-    constraint = VegetarianHardConstraint()
-    assert(not constraint.is_valid(aviad, [aviad], [hug]))
 
-def madrich_test():
-    ken = Ken()
-    ken.name = 'Misgav'
-    
-    seminar = create_seminar(1, 1)
-    hug = seminar.camps[0].hugs[0]
+class TestConstraints(unittest.TestCase):
 
-    aviad = Educative()
-    aviad.first_name = 'Aviad'
-    aviad.ken = ken
-    aviad.hug = hug
-    
-    madrich = Madrich()
-    madrich.first_name = 'Cool'
-    madrich.last_name = 'Guy'
-    madrich.ken = ken
-    madrich.hug = hug
-    
-    constraint = MadrichHardConstraint()
-    assert(not constraint.is_valid(aviad, [aviad], seminar))
+    def test_vegetarian(self):
+        aviad = Educative()
+        aviad.first_name = 'Aviad'
+        aviad.food = VEGETARIAN
+        
+        seminar = create_seminar(1, 1)
+        hug = seminar.camps[0].hugs[0]
+        hug.food = MEAT
+        aviad.hug = hug
+        
+        constraint = VegetarianHardConstraint()
+        self.assertTrue(not constraint.is_valid(aviad, [aviad], [hug]))
 
-def gender_constant_test():
-    constraint = GenderRandomSoftConstraint(10, 10, 10)
-    
-    seminar = create_seminar(1, 2)
-    hugs = seminar.camps[0].hugs
-    
-    educative1 = Educative()
-    educative1.gender = MALE
-    educative1.hug = hugs[0]
-    
-    educative2 = Educative()
-    educative2.gender = FEMALE
-    educative2.hug = hugs[0]
-    
-    educative3 = Educative()
-    educative3.gender = FEMALE
-    educative3.hug = hugs[1]
-    
-    score = constraint.calculate_score([educative1, educative2, educative3], seminar)
-    assert(score == 30)
+    def test_madrich(self):
+        ken = Ken()
+        ken.name = 'Misgav'
+        
+        seminar = create_seminar(1, 1)
+        hug = seminar.camps[0].hugs[0]
 
-def size_constant_test():
-    constraint = SizeRandomSoftConstraint(10, 10, 10)
-    
-    seminar = create_seminar(1, 2)
-    hugs = seminar.camps[0].hugs
-    
-    educative1 = Educative()
-    educative1.hug = hugs[0]
-    
-    educative2 = Educative()
-    educative2.hug = hugs[0]
-    
-    educative3 = Educative()
-    educative3.hug = hugs[1]
-    
-    score = constraint.calculate_score([educative1, educative2, educative3], seminar)
-    assert(score == 50)
+        aviad = Educative()
+        aviad.first_name = 'Aviad'
+        aviad.ken = ken
+        aviad.hug = hug
+        
+        madrich = Madrich()
+        madrich.first_name = 'Cool'
+        madrich.last_name = 'Guy'
+        madrich.ken = ken
+        madrich.hug = hug
+        
+        constraint = MadrichHardConstraint()
+        self.assertTrue(not constraint.is_valid(aviad, [aviad], seminar))
 
-def ken_constant_test():
-    constraint = KenRandomSoftConstraint(10, 10, 10)
-    
-    seminar = create_seminar(1, 2)
-    hugs = seminar.camps[0].hugs
-    
-    ken1 = Ken()
-    ken2 = Ken()
-    second_ken = SecondKen()
-    
-    educative1 = Educative()
-    educative1.ken = ken1
-    educative1.hug = hugs[0]
-    
-    educative2 = Educative()
-    educative2.ken = ken1
-    educative2.hug = hugs[0]
-    
-    educative3 = Educative()
-    educative3.ken = ken1
-    educative3.hug = hugs[1]
-    
-    educative4 = Educative()
-    educative4.ken = ken2
-    educative4.second_ken = second_ken
-    educative4.hug = hugs[1]
-    
-    score = constraint.calculate_score([educative1, educative2, educative3, educative4], seminar)
-    assert(score == 70)
+    def test_gender_constant(self):
+        constraint = GenderRandomSoftConstraint(10, 10, 10)
+        
+        seminar = create_seminar(1, 2)
+        hugs = seminar.camps[0].hugs
+        
+        educative1 = Educative()
+        educative1.gender = MALE
+        educative1.hug = hugs[0]
+        
+        educative2 = Educative()
+        educative2.gender = FEMALE
+        educative2.hug = hugs[0]
+        
+        educative3 = Educative()
+        educative3.gender = FEMALE
+        educative3.hug = hugs[1]
+        
+        score = constraint.calculate_score([educative1, educative2, educative3], seminar)
+        self.assertEquals(score, 30)
+
+    def test_size_constant(self):
+        constraint = SizeRandomSoftConstraint(10, 10, 10)
+        
+        seminar = create_seminar(1, 2)
+        hugs = seminar.camps[0].hugs
+        
+        educative1 = Educative()
+        educative1.hug = hugs[0]
+        
+        educative2 = Educative()
+        educative2.hug = hugs[0]
+        
+        educative3 = Educative()
+        educative3.hug = hugs[1]
+        
+        score = constraint.calculate_score([educative1, educative2, educative3], seminar)
+        self.assertEquals(score, 50)
+
+    def test_ken_constant(self):
+        constraint = KenRandomSoftConstraint(10, 10, 10)
+        
+        seminar = create_seminar(1, 2)
+        hugs = seminar.camps[0].hugs
+        
+        ken1 = Ken()
+        ken2 = Ken()
+        second_ken = SecondKen()
+        
+        educative1 = Educative()
+        educative1.ken = ken1
+        educative1.hug = hugs[0]
+        
+        educative2 = Educative()
+        educative2.ken = ken1
+        educative2.hug = hugs[0]
+        
+        educative3 = Educative()
+        educative3.ken = ken1
+        educative3.hug = hugs[1]
+        
+        educative4 = Educative()
+        educative4.ken = ken2
+        educative4.second_ken = second_ken
+        educative4.hug = hugs[1]
+        
+        score = constraint.calculate_score([educative1, educative2, educative3, educative4], seminar)
+        self.assertEquals(score, 70)
+
+if __name__ == '__main__':
+    unittest.main()
